fcae0ab176df2408b5a9b3a286f23cac0b39c125
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-09-23 21:41:20 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fbef14d024d0383cb962fc0869ee856d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '14bbaa3e-14c8-4b70-a8ba-a4ff602db462'
+};
